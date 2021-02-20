fbb234edde9ae2c473780461a07e93788590b3f3
Octavio Lomeli-Castro <62814754+OctavioLomeli@users.noreply.github.com>
2021-02-19 23:20:42 -0800

    Update subqueries2.sql

--- SQL Practice/Parch_Posey/subqueries2.sql
+++ SQL Practice/Parch_Posey/subqueries2.sql
@@ -1,21 +1,18 @@
 /*
 Retrieve the sales rep that has the highest sales for each region
 */
-SELECT s.name, r.name region, SUM(o.total_amt_usd)
-FROM public.accounts a
-	JOIN public.orders o ON o.account_id = a.id
-	JOIN public.sales_reps s ON s.id = a.sales_rep_id
-	JOIN public.region r ON r.id = s.region_id
-GROUP BY 1, 2
-HAVING SUM(o.total_amt_usd) IN (SELECT max
-	FROM
-		(SELECT region, MAX(sum)
-		FROM (
-			SELECT s.name, r.name region, SUM(o.total_amt_usd)
-			FROM public.accounts a
-				JOIN public.orders o ON o.account_id = a.id
-				JOIN public.sales_reps s ON s.id = a.sales_rep_id
-				JOIN public.region r ON r.id = s.region_id
-			GROUP BY 1, 2
-			)sub
-		GROUP BY 1)sub2)
+WITH rep_and_sales AS (
+	SELECT s.name, r.name region, SUM(o.total_amt_usd) sales
+	FROM public.accounts a
+		JOIN public.orders o ON o.account_id = a.id
+		JOIN public.sales_reps s ON s.id = a.sales_rep_id
+		JOIN public.region r ON r.id = s.region_id
+	GROUP BY 1, 2)
+
+SELECT name, region, sales
+FROM rep_and_sales
+	WHERE sales IN (SELECT max
+			FROM
+				(SELECT region, MAX(sales)
+				FROM rep_and_sales
+				GROUP BY 1)sub2)
