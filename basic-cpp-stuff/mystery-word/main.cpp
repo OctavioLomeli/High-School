34d48ed9cb69984375b61be9fd46325c287f6080
Octavio Lomeli-Castro <62814754+OctavioLomeli@users.noreply.github.com>
2021-05-27 19:50:50 -0700

    Update main.cpp

--- basic-cpp-stuff/mystery-word/main.cpp
+++ basic-cpp-stuff/mystery-word/main.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include <string>
 
+int rand = rnadom stuff;
+std::string mystery_word = readline(rand, filename);
+int lives = 6;
+
+
+
+
+int main()
+{
+    
+  return 0;
+  
+}
